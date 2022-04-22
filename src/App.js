fc2ec219f464e22903fc12118661ca1589119b2b
Skyler Kroll <skylerkrollz28@gmail.com>
2022-04-22 14:53:52 -0700

    Created checkout page along with checkout items and checkout cart functionality.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
 import Navigation from "./routes/navigation/Navigation.component";
-import Authentication from "./routes/authentication/Authentication";
 import Home from "./routes/home/Home.component";
-import {Routes, Route} from "react-router-dom"
 import Shop from "./routes/shop/Shop.component";
+import Authentication from "./routes/authentication/Authentication";
+import Checkout from "./routes/checkout/Checkout.component";
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path="/" element={<Navigation/>}>
-         <Route index element={<Home />}/>
-         <Route path="shop" element={<Shop />}/>
-         <Route path="auth" element={<Authentication />}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />+
+        <Route path="checkout" element={<Checkout />} />
       </Route>
-     
     </Routes>
-  )
+  );
 };
 
 export default App;
